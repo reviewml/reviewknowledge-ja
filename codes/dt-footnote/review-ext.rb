ef77cd690d1055fe75c63e6bff27850216f929de
Kenshi Muto <kmuto@debian.org>
2020-12-19 23:54:14 +0900

    dt内の脚注の特殊処理

---
+++ codes/dt-footnote/review-ext.rb
@@ -0,0 +1,38 @@
+module ReVIEW
+  module CompilerOverride
+    def compile_dlist(f)
+      @strategy.dl_begin
+      while /\A\s*:/ =~ f.peek
+        if @strategy.class.to_s =~ /LATEX/
+          s = f.gets
+          @strategy.dt(s.sub(/\A\s*:/, '').strip)
+        else
+          @strategy.dt(text(f.gets.sub(/\A\s*:/, '').strip))
+        end
+        desc = f.break(/\A(\S|\s*:|\s+\d+\.\s|\s+\*\s)/).map { |line| text(line.strip) }
+        @strategy.dd(desc)
+        f.skip_blank_lines
+        f.skip_comment_lines
+      end
+      @strategy.dl_end
+    end
+  end
+
+  class Compiler
+    prepend CompilerOverride
+  end
+
+  module LATEXBuilderOverride
+    def dt(str)
+      str.sub!(/\[/) { '\lbrack{}' }
+      str.sub!(/\]/) { '\rbrack{}' }
+      @doc_status[:caption] = true
+      puts '\item[' + compile_inline(str) + '] \mbox{} \\\\'
+      @doc_status[:caption] = nil
+    end
+  end
+
+  class LATEXBuilder
+    prepend LATEXBuilderOverride
+  end
+end
