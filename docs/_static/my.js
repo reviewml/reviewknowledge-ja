c643c332fca3e7182994f3c2de7a56bde6ccc6ff
Kenshi Muto <kmuto@kmuto.jp>
2024-05-05 16:57:55 +0900

    toggle

--- docs/_static/my.js
+++ docs/_static/my.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 $('p img').after('<br/>▲');
-$("div#e8f3c5740dfe774c11a2b84a5d3aa7cb > div > ul > li").prepend("<span class=\"toggle\">▼ </span> ");
+$("section#e8f3c5740dfe774c11a2b84a5d3aa7cb > div > ul > li").prepend("<span class=\"toggle\">▼ </span> ");
   $(".toggle").click(function() {
     $(this).parent().children("ul").toggle();
     if ($(this).parent().children("ul").is(":visible")) {
